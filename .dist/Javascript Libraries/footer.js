c897a2a2ed93f0c966eee00748b9058c0a0e97f8
Cisco <ciscophrenzy@gmail.com>
2023-06-25 14:11:59 -0600

    Add required changes

--- .dist/Javascript Libraries/footer.js
+++ .dist/Javascript Libraries/footer.js
@@ -4,8 +4,6 @@ function renderFooter() {
   const footerTemp = document.createElement('footerTemp');
   footerTemp.innerHTML = `
   <article class="partner">
-    <h3 class="section-title">Partners</h3>
-    <hr class="section-line" />
     <div class="logos">
       <img  class="partner-logo" src="images/partners/mozilla.png" , alt="mozilla-icon" />
       <img  class="partner-logo"  src="images/partners/Google.png" , alt="google-icon" />
@@ -15,7 +13,33 @@ function renderFooter() {
     </div>
   </article>
   <article class="copy-rights">
-    <img src="images/download.png" alt="cc logo" class="footer-logo" />
+  <p class="copyrights"> For further information,<br/>
+  Kindly reach out to us at <br/>
+  impact2023@techfolder.org.ng </p>
+    <img src="images/download.png" alt="cc logo" class="footer-logo" class="copyrights" />
+    <div class="copyrights">
+    <p>FOLLOW US ON SOCIAL MEDIA </p>
+    <img
+    class="tool-bar-item"
+    src="images/social/facebook.png"
+    alt="facebook-icon"
+  />
+  <img
+    class="tool-bar-item"
+    src="images/social/twitter.png"
+    alt="twitter-icon"
+  />
+  <img
+    class="tool-bar-item"
+    src="images/social/instagram.png"
+    alt="facebook-icon"
+  />
+  <img
+    class="tool-bar-item"
+    src="images/social/whatsapp.png"
+    alt="twitter-icon"
+  />
+    </div>
     <p class="copyrights">
       2022 TPPS Conference, April. <br />
       All Rights Reserved.
--- .dist/Javascript Libraries/top-bar.js
+++ .dist/Javascript Libraries/top-bar.js
@@ -1,20 +1,6 @@
 const topBar = document.querySelector('.desktop-bar');
 function renderTopBar() {
-  topBar.innerHTML = `<div class="tool-bar">
-  <img
-    class="tool-bar-item"
-    src="images/social/facebook.svg"
-    alt="facebook-icon"
-  />
-  <img
-    class="tool-bar-item"
-    src="images/social/twitter.svg"
-    alt="twitter-icon"
-  />
-  <p class="tool-bar-item">English</p>
-  <p class="tool-bar-item">My Page</p>
-  <p class="tool-bar-item last-item">Logout</p>
-</div>
+  topBar.innerHTML = `
 <nav class="navbar">
   <div class="nav-logo">
     <a href="index.html" class="logo-link"><img src="images/download.png" alt="TTPS Conference 2022" /></a>
