87a5acb7875bc3e9320426107ae3e02311cb689d
Samuel Olanrewaju <olanrewaju.samuel@giglogistics.ng>
2023-06-29 16:31:03 +0100

    adjust nav bar

--- .dist/Javascript Libraries/top-bar.js
+++ .dist/Javascript Libraries/top-bar.js
@@ -3,19 +3,17 @@ function renderTopBar() {
   topBar.innerHTML = `
 <nav class="navbar">
   <div class="nav-logo">
-    <a href="index.html" class="logo-link"><img src="images/impact_header.png" alt="IMPACT 2023 logo" /></a>
-    <p>IMPACT 2023</p>
+    <a href="index.html" class="logo-link"><img src="images/impact-logo-2023.jpeg" alt="IMPACT 2023 logo" /></a>
   </div>
   <ul class="nav-list">
     <li class="nav-item">
       <a href="about.html"} "> About </a>
     </li>
     <li class="nav-item">Program</li>
-    <li class="nav-item">Join</li>
-    <li class="nav-item">Sponsor</li>
-    <li class="nav-item">News</li>
+    <li class="nav-item">Gallery</li>
+    <li class="nav-item">Our Speakers</li>
   </ul>
-  <p class="cc-campaign">Register</p>
+  <a class="cc-campaign">Register</a>
 </nav>`;
 }
 window.onload = renderTopBar();
